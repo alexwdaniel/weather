83e649dd6ba709df91822cd53ea4680bbf16692c
Alex Daniel <alexanderwdaniel@gmail.com>
2020-03-01 09:47:32 -0800

    Hide status bar

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -73,7 +73,7 @@ struct ContentView: View {
             }
         }.onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
             self.viewModel.fetch()
-        }
+        }.statusBar(hidden: true)
     }
 }
 
